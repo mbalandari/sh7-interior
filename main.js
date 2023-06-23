27fca55b6c8a30f1d73452778c9583be42c0e0f4
codingMaz <m.balandari.toroghi@gmail.com>
2023-06-23 13:08:31 +0330

    hide the header when scrolling by if statement!

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 $(document).on('scroll', function () {
     pixlesFromTop = $(document).scrollTop()
     $('.progress').text(pixlesFromTop + ' pixels from top')
+
+    if (pixlesFromTop > 50) {
+        $('header').addClass('hidden')
+    }
+    else {
+        $('header').removeClass('hidden')
+    }
 })
