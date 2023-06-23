f79011998cf4322d42d5aaf29ce37ddf5ed2f71b
codingMaz <m.balandari.toroghi@gmail.com>
2023-06-23 13:32:57 +0330

    add different background colors when scrolling

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 $(document).on('scroll', function () {
-    pixlesFromTop = $(document).scrollTop()
-    $('.progress').text(pixlesFromTop + ' pixels from top')
+    var pixlesFromTop = $(document).scrollTop()
 
     if (pixlesFromTop > 50) {
         $('header').addClass('hidden')
     }
     else {
         $('header').removeClass('hidden')
+    }
+
+    if (pixlesFromTop < 600) {
+        $('body').css('background-color', '#fff')
+    }
+    else if (pixlesFromTop < 1400) {
+        $('body').css('background-color', '#a29c97')
+    }
+    else if (pixlesFromTop < 2200) {
+        $('body').css('background-color', '#d9dfe4')
+    }
+    else if (pixlesFromTop < 3000) {
+        $('body').css('background-color', '#fff0f0')
+    }
+    else {
+        $('body').css('background-color', '#cdccc7')
     }
 })
